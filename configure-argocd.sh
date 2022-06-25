b9fb820d9606db26ffff00c8029cab20250073aa
Mark Shields <4237425+beejiujitsu@users.noreply.github.com>
2022-06-24 23:34:08 -0400

    refactor: use -o jsonpath to get secret password

--- configure-argocd.sh
+++ configure-argocd.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 set -euxo pipefail
 
+kubectl create ns argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 ARGOCD_PORT=8080
 GIT_PORT=9418
-kubectl wait secret -n argocd argocd-initial-admin-secret --for condition=Available=True --timeout=600s
+kubectl wait secret -n argocd argocd-initial-admin-secret --for=jsonpath='{.data.password}' --timeout=300s
 kubectl port-forward -n argocd svc/argocd-server $ARGOCD_PORT:80 &
 kubectl port-forward -n argocd svc/git-server $GIT_PORT:$GIT_PORT &
 sleep 5
@@ -20,7 +21,7 @@ echo "Git Directory: $GIT_DIR"
 
 cd $GIT_DIR && mkdir test && echo $'apiVersion: v1\nkind: ConfigMap\nmetadata:\n  name: my-configmap\ndata:\n  key: value' > test/test.configmap.yaml && git add . && git commit -m "Initial commit" && git push
 
-ARGOCD_ADMIN_PW=$(kubectl get secret argocd-initial-admin-secret -n argocd -o json | jq '.data.password' -r | /usr/bin/base64 -D)
+ARGOCD_ADMIN_PW=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password} | base64 -D)
 
 BEARER_TOKEN=$(curl --insecure --fail --silent "https://localhost:${ARGOCD_PORT}/api/v1/session" -d "{\"username\":\"admin\",\"password\":\"${ARGOCD_ADMIN_PW}\"}" | jq -r '.token')
 
